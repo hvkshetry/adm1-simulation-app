8dcc01c8c41132b1d62b1b03d22dd312ea8f8218
hvkshetry <hvkshetry@gmail.com>
2025-03-15 09:01:37 -0400

    Add puran_adm1 package __init__.py

---
+++ puran_adm1/__init__.py
@@ -0,0 +1,17 @@
+"""Puran ADM1 Simulation Package
+
+This package contains the code for running ADM1 simulations for anaerobic digestion modeling.
+"""
+
+__version__ = '0.1.0'
+
+# Initialize the QSDsan components
+import qsdsan
+from qsdsan import sanunits as su, processes as pc, WasteStream, System
+from biosteam import settings
+
+# Create ADM1 components
+cmps = pc.create_adm1_cmps()
+
+# Set the thermo object
+settings.set_thermo(cmps)
